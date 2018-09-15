e353ac1bda8e5fcc32cc0c14994aff5c1362b1e1
peetonn <gpeetonn@gmail.com>
2018-09-14 18:18:15 -0700

    added missing dependency libconf

--- Formula/ndnrtc-stream.rb
+++ Formula/ndnrtc-stream.rb
@@ -13,6 +13,11 @@ class NdnrtcStream < Formula
     sha256 "49b3a825280bd66b3aa83585ef59c4a8c82f2c8a522dbe754a8bc8d08c85c491"
   end
 
+  resource "libconf" do
+    url "https://files.pythonhosted.org/packages/7c/70/e7780ef82bf2ee05a74f192083209139cd5a75404c6d5ff5c26ef1f20a93/libconf-1.0.1.tar.gz"
+    sha256 "6dd62847bb69ab5a09155cb8be2328cce01e7ef88a35e7c37bea2b1a70f8bd58"
+  end
+
   def install
     virtualenv_install_with_resources
   end
