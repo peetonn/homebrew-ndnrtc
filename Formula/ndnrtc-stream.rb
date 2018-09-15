4ac0df9a12e8edc173897703ca5ff024bca9d6ff
peetonn <gpeetonn@gmail.com>
2018-09-14 18:15:19 -0700

    fixing formula dependency on python

--- Formula/ndnrtc-stream.rb
+++ Formula/ndnrtc-stream.rb
@@ -6,7 +6,7 @@ class NdnrtcStream < Formula
   url "https://github.com/remap/ndnrtc-stream/archive/v0.0.1.tar.gz"
   sha256 "9c3c544bfb95762a7d04ce048f206d4d8b3c9a6d7bc5c6a12b2e0888d766fdaf"
 
-  depends_on "python3"
+  depends_on "python2"
 
   resource "docopt" do
     url "https://files.pythonhosted.org/packages/a2/55/8f8cab2afd404cf578136ef2cc5dfb50baa1761b68c9da1fb1e4eed343c9/docopt-0.6.2.tar.gz"
@@ -14,7 +14,6 @@ class NdnrtcStream < Formula
   end
 
   def install
-    virtualenv_create(libexec, "python3")
     virtualenv_install_with_resources
   end
 
